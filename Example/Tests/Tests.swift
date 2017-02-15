a4f5ecc9f6cba7c1b8d08b7697f99b92494783cd
Mikhail Zinov <zinov.m.v@gmail.com>
2017-02-15 15:25:37 +0300

    Update project settings

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,6 +1,5 @@
 import UIKit
 import XCTest
-import RealmDAO
 
 class Tests: XCTestCase {
     
@@ -20,10 +19,7 @@ class Tests: XCTestCase {
     }
     
     func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
+        
     }
     
 }
