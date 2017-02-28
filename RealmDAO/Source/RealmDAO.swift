d772099b2331bb567240ca7c64926ea0bb4fe274
Mikhail Zinov <zinov.m.v@gmail.com>
2017-02-28 12:15:08 +0300

    Add encryption key parameter

--- RealmDAO/Source/RealmDAO.swift
+++ RealmDAO/Source/RealmDAO.swift
@@ -15,23 +15,26 @@ import RealmSwift
 
 public class RealmDAO<Entity, Entry: Object> {
     private let translator: RealmDAOTranslator<Entity, Entry>
+    private let key: Data?
     
     /**
      Инициализатор DAO
      
      - Parameter translator: Транслятор для преобразования обьектных моделей
      в модели базы данных
+     - Parameter key: Строка. Ключ шифрования Realm. Если ключь nil, файл realm не шифруется
      
      - Returns: Реализация DAO для конкретного сервиса
      - Remark: Класс Entry должен быть унаследован от Realm Object
      */
-    public init(translator: RealmDAOTranslator<Entity, Entry>) {
+    public init(translator: RealmDAOTranslator<Entity, Entry>, key: String? = nil) {
         self.translator = translator
+        self.key = key?.data(using: .ascii)
     }
     
     public func persist(_ entity: Entity) -> Bool {
         do {
-            let realm = try Realm()
+            let realm = try self.realm()
             try realm.write {
                 realm.add(try! translator.toEntry(entity: entity), update: true)
             }
@@ -43,7 +46,7 @@ public class RealmDAO<Entity, Entry: Object> {
     
     public func persist(_ entities: [Entity]) -> Bool {
         do {
-            let realm = try Realm()
+            let realm = try self.realm()
             try realm.write {
                 realm.add(translator.toEntries(entities: entities), update: true)
             }
@@ -54,18 +57,18 @@ public class RealmDAO<Entity, Entry: Object> {
     }
     
     public func read(id: String) -> Entity? {
-        let realm = try! Realm()
+        let realm = try! self.realm()
         return try! translator.toEntity(entry: realm.object(ofType: Entry.self, forPrimaryKey: id)!)
     }
     
     public func read() -> [Entity] {
-        let realm = try! Realm()
+        let realm = try! self.realm()
         return translator.toEntities(entries: Array(realm.objects(Entry.self)))
     }
     
     public func erase(id: String) -> Bool {
         do {
-            let realm = try Realm()
+            let realm = try self.realm()
             try realm.write {
                 realm.delete(realm.object(ofType: Entry.self, forPrimaryKey: id)!)
             }
@@ -77,7 +80,7 @@ public class RealmDAO<Entity, Entry: Object> {
     
     public func erase() -> Bool {
         do {
-            let realm = try Realm()
+            let realm = try self.realm()
             try realm.write {
                 realm.delete(realm.objects(Entry.self))
             }
@@ -85,5 +88,10 @@ public class RealmDAO<Entity, Entry: Object> {
             return false
         }
         return true
+    }
+    
+    private func realm() throws -> Realm {
+        let config = Realm.Configuration(encryptionKey: self.key)
+        return try Realm(configuration: config)
     }
 }
